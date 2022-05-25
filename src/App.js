75835940949426c7ea042d2ec933d728cf2bf773
Umeshsuman <94736870+Umeshsuman@users.noreply.github.com>
2022-05-25 09:00:05 +0530

    25-5-22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar";
+import './app.css'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+import Services from "./pages/Services";
+import Products from "./pages/Products";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  export default function App(){
+    return (
+    <div>
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/services"component={Services}/>
+                <Route path="/products"component={Products}/>
+                <Route path="/sign-up"component={Signup}/>
+            </Switch>
+        </Router>
     </div>
-  );
+    );
 }
-
-export default App;
---
+++ src/pages/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../app.css'
+
+function Signup() {
+  return (
+    <h1 className='sign-up'>Signup</h1>
+  )
+}
+
+export default Signup
